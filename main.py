ceccde4a1eb6a8c618c20e5860d7859dd74373c1
student <sashulyakholin@gmail.com>
2023-12-18 20:42:13 +0200

    Innit

--- main.py
+++ main.py
@@ -18,5 +18,35 @@
 # print(a,"*",b,"=", a * b)
 # print(a,"/",b,"=", a / b)
 # print(a,"-",b,"=", a - b)
+# a = 10
+# b = 1
+# print(a+b)
 
+#1
+# a = 26
+#
+# print(a // 10)
+# print(a % 10)
+#2
+# val = 891
+#
+# a = val % 10
+# b = (val // 10)%10
+# c = a // 100
+#
+# print(a)
+# print(b)
+# print(c)
+# print(a+b+c)
+#3
+# a = int(input("Введіть число:"))
+# b = int(input("Введіть число:"))
+#
+# print(a,b)
 
+#4
+# a = int(input("Введіть температуру в градусах цельсія:"))
+#
+# b = (a*2)+32-6
+#
+# print("Приблизно дорівню:",+b)

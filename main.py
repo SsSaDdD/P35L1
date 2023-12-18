104891a2a9f284d443d4d5e452038fffbe83dc74
student <sashulyakholin@gmail.com>
2023-12-18 18:59:46 +0200

    Innit

---
+++ main.py
@@ -0,0 +1,22 @@
+# messege = input('Введіть повідомлення:')
+# print('Ваше повідомлення:',messege)
+
+
+# print("Nothing \nwill work \nunlles you do ")
+#
+# print("\"Anyone who \nstops \nlearning is old, \nwhether at twenty or eighty \" \nHenry Ford ")
+
+# a = int(input("Введіть перше число:"))
+# b = int(input("Введіть друге число:"))
+#
+# print(a + b)
+
+# a = int(input("Введіть перше число:"))
+# b = int(input("Введіть друге число:"))
+#
+# print(a,"+",b,"=", a + b)
+# print(a,"*",b,"=", a * b)
+# print(a,"/",b,"=", a / b)
+# print(a,"-",b,"=", a - b)
+
+
